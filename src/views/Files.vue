b16bf11f4634535545040afa481540adc3abc1b1
MrGaoGang <gaogangwork@qq.com>
2021-08-25 12:44:27 +0800

    fix(news): api response error

--- src/views/Files.vue
+++ src/views/Files.vue
@@ -39,7 +39,7 @@ export default {
   },
   beforeMount() {
     getAllNews().then((res) => {
-      this.list = (res || []).map((ele) => {
+      this.list = (res.data || []).map((ele) => {
         return {
           name: ele.desc,
           url: ele.url,
