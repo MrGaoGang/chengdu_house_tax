cd0161a8c235173ea469cb5f4f0440e0e7450ed5
MrGaoGang <gaogangwork@qq.com>
2021-08-27 11:28:17 +0800

    feat: add image support

--- src/views/FileDetail.vue
+++ src/views/FileDetail.vue
@@ -6,7 +6,8 @@
       left-arrow
       @click-left="onClickLeft"
     />
-    <iframe width="100%" height="100%" :src="url" frameborder="0"></iframe>
+    <img v-if="isImage(url)" />
+    <iframe v-else width="100%" height="100%" :src="url" frameborder="0"></iframe>
   </div>
 </template>
 
@@ -24,6 +25,9 @@ export default {
     onClickLeft() {
       this.$router.go(-1);
     },
+    isImage(){
+      return this.url.endsWith('jpg') || this.url.endsWith('jpeg') || this.url.endsWith('png') || this.url.endsWith('gig')
+    }
   },
 };
 </script>
